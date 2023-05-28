81fc046e1d1654aeae154f0b1504ad7a9f7676fa
ThisSilenceIsMine <aurakel542@gmail.com>
2023-05-29 01:33:21 +0300

    feat: :lipstick: Updated ui

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,16 +1,16 @@
 import type { Note } from "~/lib/types";
-
+import * as Card from "./ui/card";
 type Props = {
   note: Note;
 };
 
 export const NoteCard = ({ note }: Props) => {
   return (
-    <div className="rounded-lg bg-slate-600 p-4 text-white">
-      <h1 className="text-2xl font-bold">{note.title}</h1>
-      <p className="text-lg">{note.content}</p>
-    </div>
+    <Card.Card>
+      <Card.CardHeader>
+        <Card.CardTitle>{note.title}</Card.CardTitle>
+        <Card.CardDescription>{note.content}</Card.CardDescription>
+      </Card.CardHeader>
+    </Card.Card>
   );
 };
-
-// export const NoteCard = ({ note }: Props) => {
