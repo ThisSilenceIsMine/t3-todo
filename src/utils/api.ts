8d000d9ee059833f341c830350c63b0ed35710f6
ThisSilenceIsMine <aurakel542@gmail.com>
2023-06-01 00:57:36 +0300

    feat: :adhesive_bandage: Remove redundant http prefix in netlify urls

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,8 +15,8 @@ import { type AppRouter } from "~/server/api/root";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  if (process.env.URl) return `https://${process.env.URL!}`; // Netluiy should use netlify url
-  if (process.env.DEPLOY_URL) return `https://${process.env.DEPLOY_URL}`;
+  if (process.env.URl) return process.env.URL!; // Netluiy should use netlify url
+  if (process.env.DEPLOY_URL) return process.env.DEPLOY_URL!;
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
